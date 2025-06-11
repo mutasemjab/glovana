fe3291b1101e42673381496d0d918fe10cf00222
mutasem <mutasemms198@gmail.com>
2025-06-11 14:10:12 +0300

    api

--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -49,6 +49,19 @@
                             @enderror
                         </div>
 
+                          <div class="col-md-12">
+                                <div class="form-group">
+                                    <img src="" id="image-preview" alt="Selected Image" height="50px" width="50px"
+                                        style="display: none;">
+                                    <button class="btn"> photo</button>
+                                    <input type="file" id="Item_img" name="photo" class="form-control"
+                                        onchange="previewImage()">
+                                    @error('photo')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
                         <div class="d-flex gap-2">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('messages.Update') }}
@@ -63,4 +76,25 @@
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('script')
+    <script>
+        function previewImage() {
+            var preview = document.getElementById('image-preview');
+            var input = document.getElementById('Item_img');
+            var file = input.files[0];
+            if (file) {
+                preview.style.display = "block";
+                var reader = new FileReader();
+                reader.onload = function() {
+                    preview.src = reader.result;
+                }
+                reader.readAsDataURL(file);
+            } else {
+                preview.style.display = "none";
+            }
+        }
+    </script>
 @endsection
