6f560e40625640e9cf620d4700e8a1f6af20b46a
mutasem <mutasemms198@gmail.com>
2025-06-13 00:06:44 +0300

    api

--- app/Http/Controllers/Api/v1/User/CartController.php
+++ app/Http/Controllers/Api/v1/User/CartController.php
@@ -24,7 +24,7 @@ class CartController extends Controller
     {
         $request->validate([
             'product_id' => 'required|exists:products,id',
-            'quantity'   => 'required|integer|min:1',
+            'quantity'   => 'required',
         ]);
 
         $product = Product::find($request->product_id);
